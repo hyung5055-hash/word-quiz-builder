70e902ddf42c3d77f17ac98d84639a2d18b4fab1
hyung5055-hash <hyung5055@gmail.com>
2026-03-15 22:49:59 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "wouter";
 import Login from "./pages/Login";
 import MatchDeFr from "./pages/MatchDeFr";
-import MatchFrDeIt from "@/pages/MatchFrDeIt";
+import MatchFrDeIt from "./pages/MatchFrDeIt";
 
 function App() {
   return (
