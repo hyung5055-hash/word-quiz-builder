bbbe4e52e4a54786c6fa234e83d4af3f53aec3f0
hyung5055-hash <hyung5055@gmail.com>
2026-03-15 18:27:58 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "wouter";
 import Login from "./pages/Login";
 import MatchDeFr from "./pages/MatchDeFr";
+import MatchDeFrIt from "@/pages/MatchDeFrIt";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Route path="/" component={Login} />
       <Route path="/login" component={Login} />
       <Route path="/match" component={MatchDeFr} />
+      <Route path="/matchdefrit" component={MatchDeFrIt} />
+      
     </Switch>
   );
 }
