3a52235d956f9118ad42bb8c86ccb051a296ee5d
hyung5055-hash <hyung5055@gmail.com>
2026-03-15 18:39:26 +0900

    Update matchdefrit.ts

--- api/matchdefrit.ts
+++ api/matchdefrit.ts
@@ -1,1 +1,31 @@
+export default function handler(req, res) {
+  const data = [
+    {
+      de: "Die Katze",
+      fr: "Le chat",
+      it: "Il gatto",
+    },
+    {
+      de: "Das Haus",
+      fr: "La maison",
+      it: "La casa",
+    },
+    {
+      de: "Der Hund",
+      fr: "Le chien",
+      it: "Il cane",
+    },
+    {
+      de: "Das Buch",
+      fr: "Le livre",
+      it: "Il libro",
+    },
+    {
+      de: "Das Wasser",
+      fr: "L'eau",
+      it: "L'acqua",
+    },
+  ];
 
+  res.status(200).json(data);
+}
