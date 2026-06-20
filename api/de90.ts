adec03ccc0973bbdc72410084e7f577725f23e90
hyung5055-hash <hyung5055@gmail.com>
2026-06-20 18:34:42 +0900

    Update de90.ts

--- api/de90.ts
+++ api/de90.ts
@@ -1,1 +1,12 @@
+import fs from "fs";
+import path from "path";
 
+export default function handler(req, res) {
+  const file = path.join(process.cwd(), "client/public/de90.json");
+
+  const data = JSON.parse(
+    fs.readFileSync(file, "utf8")
+  );
+
+  res.status(200).json(data);
+}
